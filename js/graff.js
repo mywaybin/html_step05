6f318c3cea020daf855d3b782c1d06ff81e37e03
mywaybin <118698699+mywaybin@users.noreply.github.com>
2022-11-29 12:37:19 +0900

    20221129

--- js/graff.js
+++ js/graff.js
@@ -1,8 +1,20 @@
 $(function () {
 
+    // topBanner
     $('.topBanner i').on('click', function () {
         $('.topBanner').addClass('on')
-    })
+    });
+
+    // mainSlide
+    $('.mainSlide').slick({
+        //slidesToShow: 2,
+        //arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        pauseOnHover: false,
+        pauseOnFocus: false
+    });
 
 
 });
