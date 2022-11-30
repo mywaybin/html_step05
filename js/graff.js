1c7e985c87f75b94681e0361125b38e34893c986
mywaybin <118698699+mywaybin@users.noreply.github.com>
2022-11-30 14:25:28 +0900

    20221130

--- js/graff.js
+++ js/graff.js
@@ -16,5 +16,29 @@ $(function () {
         pauseOnFocus: false
     });
 
+    $('.mainSlide').on('afterChange', function (e, s, c) {
+        $('.main_dots li').eq(c).addClass('on')
+            .siblings().removeClass('on');
+        $('.mainVisual .main_num').text(c + 1)
+    });
 
-});
+    $('.main_dots li').on('click', function () {
+        var idx = $(this).index();
+        $('.mainSlide').slick('slickGoTo', idx)
+    })
+
+    $('.main_pro_slide').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 5,
+    });
+
+    $('.mainProduct .arrows .Prev').on('click', function () {
+        $('.main_pro_slide').slick('slickPrev')
+    });
+
+    $('.mainProduct .arrows .Next').on('click', function () {
+        $('.main_pro_slide').slick('slickNext')
+    });
+
+})
